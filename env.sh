84659f3f595dfb21af6670204219a434d8d7a9af
Prabhat Kumar <prabhat.genome@gmail.com>
2016-02-22 17:34:02 +0530

    Initial commit
    env.sh, shell script to --/inform/-- environments of "HIV-Pred®" in one go.
    Usage: $ sh env.sh

---
+++ env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+# ============================ HIV-Pred® =============================
+# Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+# Copyright © 2016, Prabhat Kumar. All rights reserved.
+# ====================================================================
