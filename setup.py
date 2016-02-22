2e9f2c6a61b662e82aa7c927987d1d801f1f2cdc
Prabhat Kumar <prabhat.genome@gmail.com>
2016-02-23 02:26:01 +0530

    Initial commit
    metadata of HIV-Pred®.

--- setup.py
+++ setup.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+__account__     = 'iamprabhat'
+__author__      = 'Prabhat Kumar, prabhat.genome@gmail.com'
+__githubURL__   = 'https://github.com/iamprabhat'
+__homepage__    = 'http://prabhatkumar.org/'
+__license__     = 'Apache License'
