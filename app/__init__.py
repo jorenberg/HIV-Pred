9e2d6aa711322fd803325c97a92cc69d7e6f2e47
Prabhat Kumar <prabhat.genome@gmail.com>
2016-02-25 01:29:31 +0530

    Initial commit
    License and Version of HIV-Pred®.

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,6 @@
 """HIV-Pred®, is a Machine Learning Model to predict HIV Resistance Phenotypes."""
 __author__  = 'Prabhat Kumar'
+__license__ = 'Apache License'
+__version__ = '1.0.0'
+__release__ = __version__
+# + '-dev' # comment out '-dev' before a release.
