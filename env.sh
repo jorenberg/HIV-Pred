127c6a08634326baf546c6ffd65a489a8e5ee40e
Prabhat Kumar <prabhat.genome@gmail.com>
2016-02-26 18:22:05 +0530

    Initial commit
    To check python version.

--- env.sh
+++ env.sh
@@ -5,3 +5,7 @@
 # Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
 # Copyright © 2016, Prabhat Kumar. All rights reserved.
 # ====================================================================
+
+PYV=`python -c "import sys;t='{v[0]}.{v[1]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";`
+echo "HIV-Pred® ------------"
+echo "Python version is:" $PYV
