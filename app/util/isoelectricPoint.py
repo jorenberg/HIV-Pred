41b5f60849b1d6af315766426c9936b8f2bc0f50
Prabhat Kumar <prabhat.genome@gmail.com>
2016-02-29 20:56:50 +0530

    Initial commit
    Isoelectric Points of Amino Acid(s).

--- app/util/isoelectricPoint.py
+++ app/util/isoelectricPoint.py
@@ -2,3 +2,32 @@
 # -*- coding: utf-8 -*-
 
 __author__  = 'Prabhat Kumar, prabhat.genome@gmail.com'
+
+# Isoelectric Points of Amino Acid(s).
+# ------------------------------------
+isoelectric_points = {
+    'A':06.11,
+    'R':10.76,
+    'N':10.76,
+    'D':02.98,
+    'C':05.02,
+    'E':03.08,
+    'Q':05.65,
+    'G':06.06,
+    'H':07.64,
+    'I':06.04,
+    'L':06.04,
+    'K':09.74,
+    'M':05.74,
+    'F':05.91,
+    'P':06.30,
+    'S':05.68,
+    'T':05.60,
+    'W':05.88,
+    'Y':05.63,
+    'V':06.02,
+    'X':07.00,  # unknown so assign neutral
+    'B':06.87,  # the average of D and N
+    'Z':04.35,  # the average of E and Q
+    'J':06.04,  # the average of I and L
+}
