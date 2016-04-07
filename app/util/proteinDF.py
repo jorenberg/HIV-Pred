37ace389479f37ceb9da869bb93be424307de152
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-08 00:08:58 +0530

    Initial commit
    defining "__init__", function by using a function definition.

--- app/util/proteinDF.py
+++ app/util/proteinDF.py
@@ -5,3 +5,8 @@ __author__  = 'Prabhat Kumar, prabhat.genome@gmail.com'
 
 class ProteinDataFrame(object):
     """Doc string for Protein Data Frame."""
+    def __init__(self, handle):
+        super(ProteinDataFrame, self).__init__()
+        self.handle = handle
+        self.protein_df = self.parse_pdb()
+        self.alpha_carbons = self.extract_alpha_carbons()
